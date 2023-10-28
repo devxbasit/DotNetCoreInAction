34ef3878980f8f90af56d4972de51226d1568a33
Basit <devxbasit@gmail.com>
2023-10-28 11:48:35 +0530

    http client partial commit

---
+++ RahulNathPlaylist/HttpClient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HttpClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HttpClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly AviationStackOptions _aviationStackOptions;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(IOptions<AviationStackOptions> aviationStackOptions,
+            ILogger<WeatherForecastController> logger)
+        {
+            _aviationStackOptions = aviationStackOptions.Value;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> GetData()
+        {
+            var url = new Uri(
+                $"{_aviationStackOptions.Protocol}{_aviationStackOptions.Domain}/v1/flights?access_key={_aviationStackOptions.ApiKey}");
+
+            try
+            {
+                using (var httpClient = new System.Net.Http.HttpClient())
+                {
+                    var response = await httpClient.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(100, ex, "Http Request Error");
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ RahulNathPlaylist/HttpClient/Models/AviationStackOptions.cs
@@ -0,0 +1,9 @@
+namespace HttpClient.Models
+{
+    public class AviationStackOptions
+    {
+        public string Protocol { get; set; }
+        public string Domain { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
