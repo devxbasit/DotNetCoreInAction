cf3a74767d4e5a15aae5a4c430a77fc767fbba54
Basit <devxbasit@gmail.com>
2025-01-19 00:18:45 +0530

    Grpc with json transcoding --partial commit

---
+++ GrpcWithJsonTranscoding/TodoGrpc/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoGrpc.Models;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string ToDoStatus { get; set; } = "NEW";
+}
